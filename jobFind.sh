d4b8f6057814f9595ad82235a5604b7818ba38d9
Lisa Knox <lisaknox83@gmail.com>
2022-09-16 13:36:56 -0400

    diff to file wokrs

--- jobFind.sh
+++ jobFind.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/bash
 
+
 URL='https://boards.greenhouse.io/icapitalnetwork'
 HTML=$( curl "${URL}" )
 FORMATTEDURL=${URL////_}
 
 
-echo $FORMATTEDURL
+# echo $FORMATTEDURL
 
 if [[ -f "${FORMATTEDURL}.txt" ]]
   then
     echo "${FORMATTEDURL}.txt exists"
+    diff compare.txt ${FORMATTEDURL}.txt > diff.txt
+    > compare.txt
+    > ${FORMATTEDURL}.txt
+    echo $HTML >> compare.txt
+
   else
-    echo "${FORMATTEDURL}.txt does not exist"
     touch "${FORMATTEDURL}.txt"
+    echo "${FORMATTEDURL}.txt was created"
+    echo $HTML >> ${FORMATTEDURL}.txt
 fi
+
 
 
 exit 0
