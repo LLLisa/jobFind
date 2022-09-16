20f3a5af8741b8729b664e7632e98599fbe7c50e
Lisa Knox <lisaknox83@gmail.com>
2022-09-16 11:59:29 -0400

    gitignore smh

--- parseHTML.js
+++
@@ -1,7 +0,0 @@
-const htmlToJson = require('html-to-json');
-
-console.log(process.argv.slice(2));
-
-const parsed = htmlToJson.parse(process.argv[3]).then((result) => result);
-
-console.log(parsed);
