b9fc9f4ca59e59a7ccd42e425f548c1284d76bd1
Lisa Knox <lisaknox83@gmail.com>
2022-09-16 21:07:44 -0400

    diff.txt works

--- jobFind.sh
+++ jobFind.sh
@@ -2,26 +2,17 @@
 
 
 URL='https://boards.greenhouse.io/icapitalnetwork'
+URL='localhost:1312'
 HTML=$( curl "${URL}" )
 FORMATTEDURL=${URL////_}
+NOW=$(date)
 
+echo $NOW
 
-# echo $FORMATTEDURL
-
-if [[ -f "${FORMATTEDURL}.txt" ]]
-  then
-    echo "${FORMATTEDURL}.txt exists"
-    diff compare.txt ${FORMATTEDURL}.txt > diff.txt
-    > compare.txt
-    > ${FORMATTEDURL}.txt
-    echo $HTML >> compare.txt
-
-  else
-    touch "${FORMATTEDURL}.txt"
-    echo "${FORMATTEDURL}.txt was created"
-    echo $HTML >> ${FORMATTEDURL}.txt
-fi
-
+echo $HTML > ${FORMATTEDURL}.txt
+echo $NOW >> diff.txt
+diff -u compare.txt ${FORMATTEDURL}.txt | grep -E "^\+" >> diff.txt
+cat ${FORMATTEDURL}.txt > compare.txt
 
 
 exit 0
